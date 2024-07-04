a86b65fb3715a88c58e70394e7ddb102d201e3e0
DBernardes <denis.bernardes099@gmail.com>
2024-07-04 12:12:12 -0300

    add linting

--- tests/__init__.py
+++ tests/__init__.py
@@ -5,17 +5,18 @@ This repo has the scripts for the unit tests of the Artificial Image Simualtor.
 Each script is responsable to the one of the classes contained in the AIS.
 """
 
-from . import (test_AIS_initialization,
-               test_AIS_operation,
-               test_Atmosphere,
-               test_BGI_class,
-               test_Channel,
-               test_HDR_class,
-               test_Noise_class,
-               test_PSF_class,
-               test_Sky_class,
-               test_Source,
-               test_Spectral_Energy_Distribution,
-               test_Spectral_Response,
-               test_Telescope
-               )
+from . import (
+    test_AIS_initialization,
+    test_AIS_operation,
+    test_Atmosphere,
+    test_BGI_class,
+    test_Channel,
+    test_HDR_class,
+    test_Noise_class,
+    test_PSF_class,
+    test_Sky_class,
+    test_Source,
+    test_Spectral_Energy_Distribution,
+    test_Spectral_Response,
+    test_Telescope,
+)
