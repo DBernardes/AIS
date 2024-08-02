a98265c461e290d96e70a4e30faa087b70e11e16
DBernardes <denis.bernardes099@gmail.com>
2024-08-02 16:52:22 -0300

    updating images

--- scripts/test.py
+++ scripts/test.py
@@ -1,7 +1,33 @@
-import os
-import sys
+# PLOT SPECTRAL LIBRARY WITH ATM + TELESCOPE + SPARC4
+import matplotlib.pyplot as plt
 
-from AIS.Spectral_Response import Channel
+from AIS.Artificial_Image_Simulator import Artificial_Image_Simulator
 
-ch = Channel(1)
-print(ch.channel_id)
+ccd_operation_mode = {
+    "em_mode": "Conv",
+    "em_gain": 1,
+    "preamp": 1,
+    "readout": 1,
+    "binn": 1,
+    "t_exp": 1,
+    "image_size": 100,
+}
+
+ais = Artificial_Image_Simulator(ccd_operation_mode, 1, -70)
+
+ais.create_source_sed("spectral_library", 15, (350, 1100, 1000), spectral_type="g5v")
+ais.create_sky_sed("new")
+plt.plot(ais.wavelength, ais.source_sed[0], label="G5 v")
+ais.apply_atmosphere_spectral_response()
+plt.plot(ais.wavelength, ais.source_sed[0], label="G5 v + atm")
+ais.apply_telescope_spectral_response()
+plt.plot(ais.wavelength, ais.source_sed[0], label="G5 v + atm + tel")
+ais.apply_sparc4_spectral_response("photometry")
+plt.plot(ais.wavelength, ais.source_sed, label="G5 v + atm + tel + inst")
+plt.legend()
+plt.xlabel("Wavelength (nm)")
+plt.ylabel(r"SED (photons/m$^2$/nm)")
+plt.xlim(350, 1100)
+plt.ylim(0, 1e11)
+plt.savefig("notebook/figures/spectral_library.png", dpi=300)
+plt.show()
